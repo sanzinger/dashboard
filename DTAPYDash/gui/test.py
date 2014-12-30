317fe30cbdd8d7e921d68364ef59f2b919c7830d
Stefan Anzinger <stefan.anzinger@gmail.com>
2014-12-30 21:17:19 +0100

    * Change to widget painter instead of panel to be able to paint on one panel with overlapping areas. * Add text widget * Add wrapper panel

--- DTAPYDash/gui/test.py
+++ DTAPYDash/gui/test.py
@@ -9,34 +9,25 @@ class Example(wx.Frame):
         panel.SetBackgroundColour("#000000")
         
         
-        self.rpm = widgets.RPMGauge(panel, -1, (200,350), bars=100, mode=widgets.RPMGauge.MODE_LOG, orient=widgets.RPMGauge.ORIENT_VERTICAL)
+        self.rpm = widgets.RPMGauge(bars=30, mode=widgets.RPMGauge.MODE_LOG, orient=widgets.RPMGauge.ORIENT_HORIZONTAL)
+        self.rpm2 = widgets.RPMGauge(bars=10, mode=widgets.RPMGauge.MODE_LINEAR, orient=widgets.RPMGauge.ORIENT_VERTICAL);
+        self.text1 = widgets.TextGauge((0,0), (.1,.1))
+        self.text2 = widgets.TextGauge((0,0), (.4,.1))
+        self.text2.value = "HALLO"
+        
+        self.rpmPanel = widgets.PanelWrapper(panel, -1, (300,200))
+        self.rpmPanel.addPainter(self.rpm, position=(0,-.050), size=(1,.8))
+        #self.rpmPanel.addPainter(self.rpm, position=(0,0.7), size=(1,.2))
+        #self.rpmPanel.addPainter(self.rpm2, position=(.05,.7), size=(.1,.3))
+        #self.rpmPanel.addPainter(self.rpm2, position=(.25,.7), size=(.1,.3))
+        self.rpmPanel.addPainter(self.text1, position=(.30, .7), size=(.1, .1))
+        #self.rpmPanel.addPainter(self.text2, position=(.50, .7), size=(.1, .05))
         #CenterPanel = wx.Panel(panel, -1)
         self.sld = wx.Slider(panel, -1, 75, 0, 100, (-1, -1), (250, -1), wx.SL_LABELS)
         vbox = wx.BoxSizer(wx.VERTICAL)
-        vbox.Add(self.rpm, 5, wx.EXPAND)
+        vbox.Add(self.rpmPanel, 5, wx.EXPAND)
         vbox.Add(self.sld, 1, wx.EXPAND)
         panel.SetSizer(vbox)
-        """
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        hbox = wx.BoxSizer(wx.HORIZONTAL)
-        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
-        
-        
-        hbox.Add(self.rpm, 1, wx.EXPAND)
-
-        hbox2.Add(CenterPanel, 1, wx.EXPAND)
-        hbox3.Add(self.sld, 0, wx.TOP)
-
-        CenterPanel.SetSizer(hbox3)
-
-        vbox.Add(hbox, 7, wx.EXPAND)
-        vbox.Add(hbox2, 1, wx.EXPAND)
-
-        
-        
-        panel.SetSizer(vbox)
-        """
         self.Bind(wx.EVT_CHAR_HOOK, self.onKeyUP)
         self.Bind(wx.EVT_SCROLL, self.onScroll)
         self.update()
@@ -50,10 +41,12 @@ class Example(wx.Frame):
             
     def update(self):
         self.rpm.value = float(self.sld.GetValue())/100
+        self.rpm2.value = 1-float(self.sld.GetValue())/100
+        self.text1.value = str(self.sld.GetValue()) + " rpm"
         
     def onScroll(self, event):
         self.update()
-        self.rpm.Refresh()
+        self.rpmPanel.Refresh()
         
     def onKeyUP(self, event):
         keyCode = event.GetKeyCode()
@@ -64,5 +57,5 @@ class Example(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App()
-    Example(None, 'RPM', full=True)
+    Example(None, 'RPM', full=False)
     app.MainLoop()
--- DTAPYDash/gui/widgets.py
+++ DTAPYDash/gui/widgets.py
@@ -1,28 +1,84 @@
 import wx, math
 
+class PanelWrapper(wx.Panel):
+    def __init__(self, parent, id=-1, size=(80, 110)):
+        wx.Panel.__init__(self, parent, id, size=size)
+        self.parent = parent
+        self.painters = []
+        
+        self.SetBackgroundColour('#000000')
+        self.Bind(wx.EVT_PAINT, self.onPaint)
+        self.Bind(wx.EVT_SIZE, self.onSize)
+        
+    def addPainter(self, painter, position=(0,0), size=(1.0,1.0)):
+        self.painters.append((painter, position, size))
+    
+    def onPaint(self, event):
+        dc = wx.PaintDC(self)
+        w,h = self.GetSize()
+        for i in self.painters:
+            painter, position, size = i
+            painter.setSize((size[0]*w, size[1]*h))
+            painter.setPosition((position[0]*w, position[1]*h))
+            painter.paint(dc)
+    
+    def onSize(self, event):
+        self.Refresh()
+    
 
-class RPMGauge(wx.Panel):
+class Painter:
+    def __init__(self, position=(0,0), size=(80, 110)):
+        self.size = size
+        self.position = position
+    
+    def setSize(self, size):
+        self.size = size
+    
+    def setPosition(self, position):
+        self.position = position
+    
+    
+class TextGauge(Painter):
+    def __init__(self, position=(0,0), size=(80, 110)):
+        Painter.__init__(self, position, size)
+        self.value = "asdf"
+        
+    
+    def paint(self, dc):
+        """
+        @type dc: wx.PaintDC
+        """
+        w,h = self.size
+        dc.SetTextForeground("#36ff27")
+        dc.SetFont(wx.Font(self.size[1], wx.MODERN, wx.NORMAL, wx.NORMAL, False, u'Consolas'))
+        dc.SetDeviceOrigin(self.position[0], self.position[1]+h)
+        dc.SetAxisOrientation(True, True)
+        dc.DrawText(str(self.value), self.size[0], self.size[1])
+        pass
+    
+    def setValue(self, value):
+        self.value = value
+    
+    
+        
+class RPMGauge(Painter):
     MODE_LOG = 1
     MODE_LINEAR = 2
     ORIENT_VERTICAL = 1
     ORIENT_HORIZONTAL = 2
     
-    def __init__(self, parent, id, size=(80, 110), bars=8, mode=MODE_LOG, orient=ORIENT_VERTICAL):
-        wx.Panel.__init__(self, parent, id, size=size)
-        self.parent = parent
-        self.SetBackgroundColour('#000000')
-        #self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
-        self.Bind(wx.EVT_PAINT, self.onPaint)
-        self.Bind(wx.EVT_SIZE, self.onSize)
+    def __init__(self, position=(0,0), size=(80, 110), bars=8, mode=MODE_LOG, orient=ORIENT_VERTICAL):
+        #self.size = size
+        #self.position = position
+        Painter.__init__(self, position, size)
         self.bars = bars
         self.value = 0
         self.mode = mode
         self.orientation = orient
     
-    def onPaint(self, event):
-        dc = wx.PaintDC(self)
+    def paint(self, dc):
         isVertical = self.orientation == RPMGauge.ORIENT_VERTICAL
-        w,h = self.GetSize()
+        w,h = self.size
         
         if self.mode == RPMGauge.MODE_LOG:
             x1 = lambda x: 1.0-math.log(1.2)/math.log(1.2+1*x)
@@ -31,7 +87,15 @@ class RPMGauge(wx.Panel):
             x1 = lambda x: 0.0
             x2 = lambda x: 1.0
         
-        dc.SetDeviceOrigin(0, h)
+        def colorBars(x):
+            if x > redArea:
+                return "#FF0000" if value > x else "#730000"
+            if x > yellowArea:
+                return "#FFFF00" if value > x else "#808000"
+            else:
+                return "#36ff27" if value > x else "#075100"
+            
+        dc.SetDeviceOrigin(self.position[0], self.position[1]+h)
         dc.SetAxisOrientation(True, True)
         value = self.value
         redArea = .8
@@ -41,13 +105,6 @@ class RPMGauge(wx.Panel):
         barDistance = ((w*(1+barSpaceRatio/self.bars) / self.bars))
         barWidth = ((barDistance * (1.0-barSpaceRatio)))
         
-        def colorBars(x):
-            if x > redArea:
-                return "#FF0000" if value > x else "#730000"
-            if x > yellowArea:
-                return "#FFFF00" if value > x else "#808000"
-            else:
-                return "#36ff27" if value > x else "#075100"
         color = colorBars
         for i in range(0, self.bars):
             prc = .0001 + float(i+.5)/self.bars
@@ -58,7 +115,4 @@ class RPMGauge(wx.Panel):
                 dc.DrawRectangle(bottom, i*barDistance, top, barWidth)
             else:
                 dc.DrawRectangle(i*barDistance, bottom, barWidth, top)
-        
-    def onSize(self, event):
-        self.Refresh()
   
